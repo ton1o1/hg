8c0de8fb89c2790f0c47d9ad1a0b6f0abb26d078
TangoMan75 <matthias.morin@gmail.com>
2015-12-04 12:52:08 +0100

    commit

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-// v2.3
+// v2.4
 // Formulaire de connexion
 // Changelog: 11:59 04/12/2015 : Ajout du lien lost_password.php
 
@@ -10,7 +10,6 @@ session_start();
 if(!empty($_SESSION['auth'])){  die( header('Location: ./') ); }
 
 require_once 'inc/pdo.php';
-require_once 'func/keyGenerator.php';
 
 if($_POST){
 
