db780191d447d8e7dfc03f7bfde81edb1420a3a9
ton1o1 <ton1o1@users.noreply.github.com>
2015-12-04 13:04:46 +0100

    Ajout d'un logement

--- lodging_add.php
+++ lodging_add.php
@@ -1,1 +1,51 @@
-<!-- Formulaire d'ajout de logement -->
+<?php
+// Formulaire d'ajout de logement
+
+// Démarrage de la session
+session_start();
+
+// Si l'user n'est pas connecté, on redirige sur la home
+if(empty($_SESSION['auth'])){  die( header('Location: ./') ); }
+
+require_once 'inc/pdo.php';
+
+if($_POST){
+
+// Le formulaire est-il soumis ?
+if ( !empty($_POST['lodging_add']['submit']) ) {
+
+    // Si le tous les champs du formulaire sont remplis
+    if ( !empty($_POST['lodging_add']['address']) && !empty($_POST['lodging_add']['zipcode']) && !empty($_POST['lodging_add']['city']) && !empty($_POST['lodging_add']['capacity']) ) {
+
+    	// Enregistrement du logement dans la database
+        $query = $pdo->prepare("INSERT INTO lodging VALUES('', :userId, :address, :zipcode, :city, :capacity);");
+        $query->execute([
+            ':userId' => $_SESSION['auth']['id'],
+            ':address' => $_POST['address'],
+            ':zipcode' => $_POST['zipcode'],
+            ':city' => $_POST['city'],
+            ':capacity' => $_POST['capacity'],
+        ]);
+
+    } else $error = "Tous les champs doivent être remplis.";
+
+} else $error = "Le formulaire n'a pas été correctement validé.";
+
+}
+
+$title = 'Ajout d\'un logement';
+require_once './view/header.php';
+
+// Si on a une erreur à afficher
+if ( !empty($error) ) {
+    echo '<h2 style="color:red">'.$error.'</h2>';
+}
+?>
+<form method="post">
+    <input type="text" placeholder="Adresse" name="lodging_add[address]" value="<?=!empty($_POST['lodging_add']['address']) ? $_POST['lodging_add']['address'] : ''?>" required />
+    <br /><input type="text" placeholder="Code postal" name="lodging_add[zipcode]" value="<?=!empty($_POST['lodging_add']['zipcode']) ? $_POST['lodging_add']['zipcode'] : ''?>" required />
+    <br /><input type="text" placeholder="Ville" name="lodging_add[city]" value="<?=!empty($_POST['lodging_add']['city']) ? $_POST['lodging_add']['city'] : ''?>" required />
+    <br /><input type="text" placeholder="Capacité d'accueil" name="lodging_add[capacity]" value="<?=!empty($_POST['lodging_add']['capacity']) ? $_POST['lodging_add']['capacity'] : ''?>" required />
+    <br /><input type="submit" name="lodging_add[submit]" value="Ajouter" />
+</form>
+<?php require_once './view/footer.php'; ?>
