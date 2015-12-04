7469f3240598454a4f619917735d9fa9f25aff25
TangoMan75 <matthias.morin@gmail.com>
2015-12-04 17:30:46 +0100

    Je COMMIIIIIT un ketru ki marche!!!!!!

--- register.php
+++ register.php
@@ -1,99 +1,106 @@
 <?php
-	// register.php
-    // v2.9
-	// Changelog: 15:25 04/12/2015 : Affichage d'un message en cas de succès de création de compte
-	// Changelog: 15:25 04/12/2015 : Ajout d'une étape de validation d'email
-	// Changelog: 10:32 04/12/2015 : Ajout meta charset
-	// Changelog: 10:09 04/12/2015 : Ajout de la variable $error
+    // register.php
+    // v3.1
+    // Changelog: 16:47 04/12/2015 : Ajout d'une étape de vérification d'utilisateur déjà enregistré avec le même email
+    // Changelog: 15:25 04/12/2015 : Ajout d'une étape de validation d'email
+    // Changelog: 10:32 04/12/2015 : Ajout meta charset
+    // Changelog: 10:09 04/12/2015 : Ajout de la variable $error
 
-	require_once 'inc/pdo.php';
-	require_once 'func/keyGenerator.php';
+    require_once 'inc/pdo.php';
+    require_once 'func/keyGenerator.php';
 
-	$error = "";
+    $error = "";
 
-	if ( $_POST ) {
+    if ( $_POST ) {
 
-	    // Quand le bouton submit est cliqué
-	    if ( !empty($_POST['register']['submit']) ) {
+        // Quand le bouton submit est cliqué
+        if ( !empty($_POST['register']['submit']) ) {
 
-	        // On vérifie que les champs ne sont pas vides
-	        if ( !empty($_POST['register']['lastname']) && !empty($_POST['register']['firstname']) && !empty($_POST['register']['email']) && !empty($_POST['register']['password']) ) {
+            // On vérifie que les champs ne sont pas vides
+            if ( !empty($_POST['register']['lastname']) && !empty($_POST['register']['firstname']) && !empty($_POST['register']['email']) && !empty($_POST['register']['password']) ) {
 
-	        	// Verification des entrées utilisateur
-	        	if ( !filter_var($_POST['register']['email'], FILTER_VALIDATE_EMAIL) ) {
+                // On vérifie que l'email saisi est valide
+                if ( filter_var($_POST['register']['email'], FILTER_VALIDATE_EMAIL) ) {
 
-	        		// L'email entré par l'utilisateur n'est pas valide
-	        		$error = "Vous devez entrer un email valide.<br />";
-	        	} else {
+                    // On confirme que les mots de passe correspondent
+                    if ( $_POST['register']['password'] == $_POST['register']['password_confirm'] ) {
 
-		        	// Génération d'un salt
-		            $salt = keyGenerator();
+                        // On vérifie qu'il n'existe pas déjà un utilisateur avec le même email dans la base
+                        $query = $pdo->prepare("SELECT COUNT(*) AS verif_email FROM user WHERE email = :email;");
+                        $query->execute([
+                            ':email' => $_POST['register']['email'],
+                            ]);
+                        $result = $query->fetch();
 
-		            try{
-			            // Mise à jour des données dans la base
-			            $statement = $pdo->prepare("INSERT INTO user VALUES ('', :password, :salt, :firstname, :lastname, :email, :phone);");
-			            $statement->execute([
+                        // $_SESSION['info'] = $result['verif_email'];
+                        // die( header('Location: ./') );
+                        if ( $result['verif_email'] == 0 ) {
 
-			            	// Hashage et ajout du hash du mot de passe dans la base
-			                ':password'  => hash('sha512', $_POST['register']['password'].$salt),
+                            // On rajoute l'utilisateur dans la base
+                            // Génération d'un salt
+                            $salt = keyGenerator();
+                            try {
+                                // Mise à jour des données dans la base
+                                $statement = $pdo->prepare("INSERT INTO user VALUES ('', :password, :salt, :firstname, :lastname, :email, :phone);");
+                                $statement->execute([
 
-			                // Ajout du salt et des autres infos de l'utilisateur
-			                ':salt'      => $salt,
-			                ':firstname' => $_POST['register']['firstname'],
-			                ':lastname'  => $_POST['register']['lastname'],
-			                ':email'     => $_POST['register']['email'],
-			                ':phone'     => $_POST['register']['phone'],
-			                ]);
-		            	$success = "Votre compte à bien été crée.";
-		            }
-		            catch (PDOException $e) {
-		            	// Il s'est produit une erreur PDO
-				        $error = 'Erreur : ' . $e->getMessage();
-		            	$success = "";
-				    }
-	        	}
+                                    // Hashage et ajout du hash du mot de passe dans la base
+                                    ':password'  => hash('sha512', $_POST['register']['password'].$salt),
 
-	        	// On ouvre une session
-	            $query = $pdo->prepare("SELECT * FROM user WHERE email = :email;");
-	            $query->execute([
-	                ':email' => $_POST['register']['email'],
-	                ]);
-	            $user = $query->fetch();
+                                    // Ajout du salt et des autres infos de l'utilisateur
+                                    ':salt'      => $salt,
+                                    ':firstname' => $_POST['register']['firstname'],
+                                    ':lastname'  => $_POST['register']['lastname'],
+                                    ':email'     => $_POST['register']['email'],
+                                    ':phone'     => $_POST['register']['phone'],
+                                    ]);
 
-	            if ( $user ) {
-	    			session_start();
+                                // On ouvre une session
+                                $query = $pdo->prepare("SELECT * FROM user WHERE email = :email;");
+                                $query->execute([
+                                    ':email' => $_POST['register']['email'],
+                                    ]);
 
-	                $_SESSION['auth'] = $user;
-	                die( header('Location: ./') );
+                                // On charge toutes les infos de l'utilisateur dans la variable $_SESSSION depuis la base
+                                $user = $query->fetch();
+                                if ( $user ) {
+                                    session_start();
+                                    $_SESSION['auth'] = $user;
+                                    $_SESSION['info'] .= "Votre compte à bien été crée.";
+                                    // On redirige l'utilisateur vers la home
+                                    die( header('Location: ./') );
+                                }
+                            }
+                            catch (PDOException $e) {
+                                // Il s'est produit une erreur PDO
+                                $error = 'Erreur : ' . $e->getMessage();
+                            }
+                        } else $error = "Un utilisateur est déjà enregistré avec cet email.";
+                    } else $error = "Les mots de passe ne correspondent pas";
+                } else $error = "Vous devez entrer un email valide.";
+            } else $error = "Veuillez renseigner tous les champs !";
+        } else $error = "Le formulaire n'a pas été correctement validé.";
+    }
 
-	            } else $error .= "Erreur d'ouverture de session.</br>";
-	        } else $error .= "Veuillez renseigner tous les champs !</br>";
-	    } else $error .= "Le formulaire n'a pas été correctement validé.</br>";
-	}
+    // inclusion du header
+    $title = 'Création de compte';
+    require_once './view/header.php';
 
-	// inclusion du header
-	$title = 'Création de compte';
-	require_once './view/header.php';
-
-	// Si on a une erreur à afficher
-	if ( !empty($error) ) {
-	    echo '<h2 style="color:red">'.$error.'</h2>';
-	}
-	// Si on a un message à afficher
-	if ( !empty($succes) ) {
-	    echo '<h2 style="color:green">'.$succes.'</h2>';
-	}
+    // Si on a une erreur à afficher
+    if ( !empty($error) ) {
+        echo '<h2 style="color:red">'.$error.'</h2>';
+    }
 ?>
 
 <!-- formulaire -->
 <form method="post">
-	<input type="text"     placeholder="Nom"          name="register[lastname]"         value="<?=!empty($_POST['register']['lastname'])  ? $_POST['register']['lastname']  : ''?>" required=""/><br />
-	<input type="text"     placeholder="Prénom"       name="register[firstname]"        value="<?=!empty($_POST['register']['firstname']) ? $_POST['register']['firstname'] : ''?>" required=""/><br />
-	<input type="email"    placeholder="Email"        name="register[email]"            value="<?=!empty($_POST['register']['email'])     ? $_POST['register']['email']     : ''?>" required=""/><br />
-	<input type="text"     placeholder="Téléphone"    name="register[phone]"            value="<?=!empty($_POST['register']['phone'])     ? $_POST['register']['phone']     : ''?>" required=""/><br />
-	<input type="password" placeholder="Mot de passe" name="register[password]"         value="<?=!empty($_POST['register']['password'])  ? $_POST['register']['password']  : ''?>" required=""/> <br />
-	<input type="password" placeholder="Confirmation" name="register[password_confirm]" value="<?=!empty($_POST['register']['password_confirm']) ? $_POST['register']['password_confirm'] : ''?>" required=""/> <br />
-	<input type="submit" name="register[submit]" value="Créer un compte"/>
+    <input type="text"     placeholder="Nom"          name="register[lastname]"         value="<?=!empty($_POST['register']['lastname'])  ? $_POST['register']['lastname']  : ''?>" required=""/><br />
+    <input type="text"     placeholder="Prénom"       name="register[firstname]"        value="<?=!empty($_POST['register']['firstname']) ? $_POST['register']['firstname'] : ''?>" required=""/><br />
+    <input type="email"    placeholder="Email"        name="register[email]"            value="<?=!empty($_POST['register']['email'])     ? $_POST['register']['email']     : ''?>" required=""/><br />
+    <input type="text"     placeholder="Téléphone"    name="register[phone]"            value="<?=!empty($_POST['register']['phone'])     ? $_POST['register']['phone']     : ''?>" required=""/><br />
+    <input type="password" placeholder="Mot de passe" name="register[password]"         value="<?=!empty($_POST['register']['password'])  ? $_POST['register']['password']  : ''?>" required=""/> <br />
+    <input type="password" placeholder="Confirmation" name="register[password_confirm]" value="<?=!empty($_POST['register']['password_confirm']) ? $_POST['register']['password_confirm'] : ''?>" required=""/> <br />
+    <input type="submit" name="register[submit]" value="Créer un compte"/>
 </form>
 
 <!-- inclusion du footer -->
