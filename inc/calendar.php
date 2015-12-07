3c4aaee2b21642f29449dd347d31bb766320fd7c
ton1o1 <ton1o1@users.noreply.github.com>
2015-12-07 11:47:31 +0100

    Calendar

--- inc/calendar.php
+++ inc/calendar.php
@@ -1,21 +1,8 @@
 <?php
-// header('Access-Control-Allow-Methods: *');
-// header('Access-Control-Allow-Credentials: true');
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Headers: Content-Type, Content-Language, Authorization');
-// header('Access-Control-Expose-Headers: Authorization');
 
 require_once 'pdo.php';
 require_once '../func/datesArray.php';
 
-// # ACTION
-// $keys = isset($_REQUEST['keys'])?$_REQUEST['keys']:array();
-// if (!is_array($keys)){
-// 	$keys = array($keys);
-// }
-// $keys = array_filter($keys);
-
-# RESULT
 $unavailable = array();
 
 	$query = $pdo->prepare("SELECT * FROM booking WHERE lodging_id = :id");
