4bc367e440dc74e327c4aea17aec34864f37a97d
SweetJeez <etienne.dutour@gmail.com>
2015-12-17 10:02:53 +0100

    prop1

--- lodging_search_app.php
+++ lodging_search_app.php
@@ -10,7 +10,7 @@ $statementCities->execute();
 
 $cities = $statementCities->fetchAll();
 
-//********************************************* DATA : pour le AJAX *****************************************
+//************************************** DATA : pour le Build_allFlats/AJAX *****************************************
 $flatsForm ='';
 
 if (!empty($_GET))
@@ -18,24 +18,60 @@ if (!empty($_GET))
 	$flatsForm = $_GET['flatsForm'];
 }
 
+if ( !empty($_GET['page']) ) 
+{
+	$page = $_GET['page'];
+}
+else
+{
+	$page = 1;
+}
+
+$pageSize = 2;
+$offset = ( $page - 1 ) * $pageSize;
+
+
 if ( strlen($flatsForm) < 3 )
 {
-	$statement = $pdo->prepare('SELECT user.firstname, user.lastname, lodging.address, lodging.id 
-								FROM lodging JOIN user ON lodging.user_id = user.id ;' );
+	$statement = $pdo->prepare( sprintf (
+								'SELECT user.firstname, user.lastname, lodging.address, lodging.id 
+								FROM lodging JOIN user ON lodging.user_id = user.id
+								ORDER BY id DESC LIMIT %1$u, %2$u ;',
+								$offset,
+								$pageSize
+								));
 	$statement->execute(); 
 } 
 else
-{
-	$statement = $pdo->prepare('SELECT user.firstname, user.lastname, lodging.address, lodging.id 
+{	
+	$statement = $pdo->prepare( sprintf (
+								'SELECT user.firstname, user.lastname, lodging.address, lodging.id 
 								FROM lodging JOIN user ON lodging.user_id = user.id 
-								WHERE city LIKE :research ;' );
-	$statement->execute([ 
-		':research' => '%' . $flatsForm . '%'
-		]); 
+								WHERE city LIKE :research 
+								ORDER BY id DESC LIMIT %1$u, %2$u ;',
+								$offset,
+								$pageSize
+								));
+	$statement->execute([
+		':research' => '%' . $flatsForm . '%' 
+		]);
 }
 
 $flats = $statement->fetchAll();
-	
+
+//************************************** DATA : pour le count *****************************************
+
+
+$statementCount = $pdo->prepare(' SELECT COUNT(*) AS count FROM lodging; ');
+$statementCount->execute();
+
+$flatsCount = $statementCount->fetchAll();
+
+// echo"<pre>";
+// print_r($flatsCount);
+
+//***********************************************************************************************************
+
 if (!empty($_GET))
 {
 		echo build_allFlats($flats);
@@ -44,7 +80,7 @@ else
 {
 	return [
 		"cities" => $cities,
-		"flats" => $flats
+		"flats" => $flats,
+		"total" => $flatsCount
 	];
 }
-
